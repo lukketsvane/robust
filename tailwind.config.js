b5f6c0ce2babc39553ab07677e9f3b8ec1dc3a7d
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-12-25 21:52:45 +0000

    YRS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      // Add any other relevant directories
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
