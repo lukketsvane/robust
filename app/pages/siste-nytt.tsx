803a77c4d764bece7817db194bf47b37b668063c
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-12-25 23:46:39 +0000

    yes

--- app/pages/siste-nytt.tsx
+++ app/pages/siste-nytt.tsx
@@ -1,48 +1,27 @@
-// siste-nytt.tsx
 "use client";
-
-import Image from 'next/image';
-import Link from 'next/link';
+import { useEffect } from 'react';
+import HeroTitle from '../components/HeroTitle';
+import AboutHero from '../components/AboutHero';
 
 const SisteNytt = () => {
+  useEffect(() => {
+    // Apply the yellow background color globally
+    document.body.style.backgroundColor = "#F2C744";
+
+    // Clean up the effect by setting the color back on component unmount
+    return () => {
+      document.body.style.backgroundColor = '';
+    };
+  }, []);
+
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold mb-8">Siste Nytt</h1>
-      <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {/* News Item 1 */}
-        <div className="rounded-lg shadow-lg p-4">
-          <Image src="/images/news1.jpg" alt="Nyhetsartikkel 1" width={350} height={200} className="rounded-lg" />
-          <h2 className="text-2xl font-bold mt-4">Nyhetsartikkel 1: Seminar om Bærekraft</h2>
-          <p className="mt-2">
-            Oppsummering og høydepunkter fra vårt siste seminar om bærekraftig økonomi og samfunn.
-          </p>
-          <Link href="/siste-nytt/nyhetsartikkel1"><a className="text-blue-500 hover:text-blue-600 mt-4 inline-block">Les mer</a></Link>
-        </div>
-
-        {/* News Item 2 */}
-        <div className="rounded-lg shadow-lg p-4">
-          <Image src="/images/news2.jpg" alt="Nyhetsartikkel 2" width={350} height={200} className="rounded-lg" />
-          <h2 className="text-2xl font-bold mt-4">Nyhetsartikkel 2: Nytt Forskningsprosjekt</h2>
-          <p className="mt-2">
-            Introduksjon til vårt nye forskningsprosjekt om nedvekst og økonomisk transformasjon.
-          </p>
-          <Link href="/siste-nytt/nyhetsartikkel2"><a className="text-blue-500 hover:text-blue-600 mt-4 inline-block">Les mer</a></Link>
-        </div>
-
-        {/* News Item 3 */}
-        <div className="rounded-lg shadow-lg p-4">
-          <Image src="/images/news3.jpg" alt="Nyhetsartikkel 3" width={350} height={200} className="rounded-lg" />
-          <h2 className="text-2xl font-bold mt-4">Nyhetsartikkel 3: Partnerskap for Bærekraft</h2>
-          <p className="mt-2">
-            Foreningen Robust inngår et spennende nytt partnerskap for å fremme bærekraftige løsninger.
-          </p>
-          <Link href="/siste-nytt/nyhetsartikkel3"><a className="text-blue-500 hover:text-blue-600 mt-4 inline-block">Les mer</a></Link>
-        </div>
-
-        {/* More news items can be added here */}
-      </div>
+    // Ensuring the div stretches to at least the height of the viewport with the correct background color
+    <div className="flex flex-col items-center justify-center min-h-screen w-full" style={{ backgroundColor: '#F2C744' }}>
+      <HeroTitle />
+      <AboutHero /> {/* Ensure any added components are contained within the main div */}
+      {/* More content can be added here with the proper background color */}
     </div>
   );
-};
+}
 
 export default SisteNytt;
--- pages/siste-nytt.tsx
+++ pages/siste-nytt.tsx
@@ -1,10 +1,25 @@
 "use client";
+import { useEffect } from 'react';
 import HeroTitle from '../app/components/HeroTitle';
 import AboutHero from '../app/components/AboutHero';
 
 const SisteNytt = () => {
+  useEffect(() => {
+    // Apply the yellow background color globally
+    document.body.style.backgroundColor = "#F2C744";
+
+    // Clean up the effect by setting the color back on component unmount
+    return () => {
+      document.body.style.backgroundColor = '';
+    };
+  }, []);
+
   return (
-    <div>
+    // Ensuring the div stretches to at least the height of the viewport with the correct background color
+    <div className="flex flex-col items-center justify-center min-h-screen w-full" style={{ backgroundColor: '#F2C744' }}>
+      <HeroTitle />
+      <AboutHero /> {/* Ensure any added components are contained within the main div */}
+      {/* More content can be added here with the proper background color */}
     </div>
   );
 }
