1c946f4e3dd8a98552493d10fa38fab0c53bea37
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-12-25 23:51:50 +0000

    sure

--- app/components/Stories.tsx
+++ app/components/Stories.tsx
@@ -5,11 +5,11 @@ import { motion } from 'framer-motion';
 
 const Stories = () => {
   return (
-    <div className="container mx-auto py-16 px-8"> {/* Doubled the padding */}
+    <div className="container mx-auto py-16 px-8">
       <h2 className="text-2xl font-semibold mb-6">Utvalgte Historier</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        {/* Kort 1 */}
+        {/* Card 1 */}
         <motion.div
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -26,17 +26,16 @@ const Stories = () => {
           />
           <div className="p-6">
             <h3 className="text-xl font-semibold mb-2">Nytt Arbeid — Robust</h3>
-            <p className="text-gray-600 mb-4">
+            <p className="text-gray-600 mb-4 line-clamp">
               Vi har samarbeidet tett med norske ingeniører og oppfinnere hos Robust for å utvikle en digital merkevareopplevelse for lanseringen av deres innovative lunsjboks.
-              Senape har ledet designarbeidet og samarbeidet med våre naboer i Design District og teknologipartneren ON for å levere markedsføringssiden, nettbutikken og den tilkoblede appen. Vi håper du liker det :)
             </p>
             <Link href="/case-study/robust">
-              <span className="cursor-pointer text-indigo-600 hover:text-indigo-800 transition duration-150 ease-in-out">Les Casestudien</span>
+              <a className="cursor-pointer text-indigo-600 hover:text-indigo-800 transition duration-150 ease-in-out">Les Casestudien</a>
             </Link>
           </div>
         </motion.div>
 
-        {/* Kort 2 */}
+        {/* Card 2 */}
         <motion.div
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -47,17 +46,17 @@ const Stories = () => {
             src="https://source.unsplash.com/random/400x301"
             alt="Utvalgt historie"
             width={400}
-            height={300}
+            height={301}
             layout="responsive"
             className="object-cover"
           />
           <div className="p-6">
             <h3 className="text-xl font-semibold mb-2">36 Dager med Type</h3>
-            <p className="text-gray-600 mb-4">
-              Vårt designteam har forpliktet seg til et tredje år med 36 dager med Type. Dette prosjektet inviterer designere, illustratører og visuelle kunstnere til å uttrykke sin unike tolkning av bokstaver og tall i det latinske alfabetet over 36 påfølgende dager. Å være en del av et talentfullt og entusiastisk designfellesskap er utrolig gøy og hjelper oss med å utvikle nye ferdigheter. Sjekk ut bidraget vårt på vår Instagram-kanal!
+            <p className="text-gray-600 mb-4 line-clamp">
+              Vårt designteam har forpliktet seg til et tredje år med 36 dager med Type, et prosjekt som inviterer designere til å uttrykke sin tolkning av bokstaver og tall i alfabetet.
             </p>
             <Link href="/design-prosess">
-              <span className="cursor-pointer text-indigo-600 hover:text-indigo-800 transition duration-150 ease-in-out">Designprosessen</span>
+              <a className="cursor-pointer text-indigo-600 hover:text-indigo-800 transition duration-150 ease-in-out">Designprosessen</a>
             </Link>
           </div>
         </motion.div>
