a746e319382ed2db78fd40c30050df2206c2ef8b
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-12-26 13:53:08 +0000

    yes

--- app/components/FingerFooter.tsx
+++ app/components/FingerFooter.tsx
@@ -1,26 +1,25 @@
-"use client"; // Ensure client-side rendering for hooks and framer-motion
+"use client";
 import { motion } from 'framer-motion';
 import React from 'react';
-import Link from 'next/link'; // Import the Link component
+import Link from 'next/link';
 
 const FingerFooter = () => {
-  // Define the animation variants for the finger graphic
   const fingerVariants = {
-    hover: { y: -10 }, // Moves up 10 pixels on hover
-    rest: { y: 0 }, // Initial position
+    hover: { y: -10 },
+    rest: { y: 0 },
   };
 
   return (
-    <div className="h-screen flex justify-center items-center" style={{ backgroundColor: '#FADA5E' }}>
+    <div className="h-screen flex justify-center items-center">
       <div className="text-center">
-        <h1 className="text-6xl font-bold mb-6" style={{ color: '#333333' }}>bli medlem!</h1>
-        <Link href="/contact" passHref> {/* Wrap the button with Link */}
+        <h1 className="text-6xl font-bold mb-6">Meld deg inn!</h1>
+        <Link href="/pages/blimedlem" passHref>
           <motion.a
             className="py-2 px-4 bg-transparent text-3xl font-semibold rounded-full border-2 border-black cursor-pointer"
             whileHover={{ scale: 1.05, boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.2)" }}
             style={{ color: '#333333', boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.1)" }}
           >
-            TITTEI
+            Bli Medlem
           </motion.a>
         </Link>
         <motion.div
