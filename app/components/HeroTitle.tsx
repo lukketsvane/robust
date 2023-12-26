b7c7ea6f21b142871ccd5771a210484b9fcf8a65
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-12-26 13:54:48 +0000

    yes

--- app/components/HeroTitle.tsx
+++ app/components/HeroTitle.tsx
@@ -3,12 +3,10 @@ import React from 'react'; // Import React to ensure it's in scope for JSX
 
 const HeroTitle = () => {
     return (
-        // Apply consistent padding class
         <div className="min-h-screen flex items-center justify-start px-4 sm:px-6 md:px-[10%] lg:px-[15%] xl:px-[20%]">
             <h1 className="title text-5xl sm:text-6xl md:text-7xl font-extrabold text-black text-left leading-tight">
-                Håndverk for bærekraft<br />
-                skapt med menneskelig varme<br />
-                i Norges ånd.
+                Mot en robust fremtid —<br />
+                for et bærekraftig Norge.
             </h1>
         </div>
     );
