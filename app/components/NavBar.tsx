f024ea5c52ed6a0555474345415aa921b111759d
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-12-26 12:06:06 +0000

    yes

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,17 @@
+// NavBar.tsx
 "use client";
-import { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { useState, useEffect } from 'react';
+import { motion, AnimatePresence, useAnimation } from 'framer-motion';
 import { MenuIcon, XIcon } from 'lucide-react';
 import Link from 'next/link';
+
+// Define the textColorForSection variable here
+const textColorForSection: { [key: string]: string } = {
+  '#617864': 'text-white',
+  '#4324D2': 'text-white',
+  '#FFFFFF': 'text-black',
+  '#F2C744': 'text-black',
+};
 
 interface NavBarProps {
   currentSection: number;
@@ -18,20 +27,36 @@ const NavBar = ({ currentSection, sectionColors }: NavBarProps) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuOverlayBackgroundColor = sectionColors?.[currentSection] || 'transparent';
 
+  const isTextWhite = textColorForSection[sectionColors[currentSection]] === 'text-white';
+
+  const iconAnimation = useAnimation();
+
+  useEffect(() => {
+    const bgColor = sectionColors[currentSection];
+    document.body.style.backgroundColor = bgColor;
+
+    iconAnimation.start({ rotate: isMenuOpen ? 0 : 180 });
+  }, [currentSection, sectionColors, iconAnimation, isMenuOpen]);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-      <nav className={`fixed top-0 left-0 right-0 z-50 py-2 ${isMenuOpen ? 'bg-transparent' : sectionColors[currentSection]} px-4 sm:px-6 lg:px-8 flex items-center justify-between`}>
+      <nav className={`fixed top-0 left-0 right-0 z-50 py-2 ${isMenuOpen ? 'bg-transparent' : sectionColors[currentSection]} px-4 sm:px-6 lg:px-8 flex items-center justify-between`}
+        style={{ transition: 'background-color 0.35s' }}>
         <Link href="/">
-          <span className="font-bold cursor-pointer text-5xl z-50">S.</span>
+          <span className={`font-bold cursor-pointer text-5xl z-50 ${isTextWhite ? 'text-white' : 'text-black'}`}>S.</span>
         </Link>
 
         <motion.button
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          onClick={toggleMenu}
           className="focus:outline-none z-50"
-          animate={{ rotate: isMenuOpen ? 180 : 0 }}
+          animate={iconAnimation}
           transition={{ type: 'spring', stiffness: 260, damping: 20 }}
         >
-          {isMenuOpen ? <XIcon className="w-10 h-10" /> : <MenuIcon className="w-10 h-10" />}
+          {isMenuOpen ? <XIcon className={`w-10 h-10 ${isTextWhite ? 'text-white' : 'text-black'}`} /> : <MenuIcon className={`w-10 h-10 ${isTextWhite ? 'text-white' : 'text-black'}`} />}
         </motion.button>
       </nav>
 
@@ -48,21 +73,21 @@ const NavBar = ({ currentSection, sectionColors }: NavBarProps) => {
           >
             <div className="pt-52 pl-4 pr-4 sm:pl-6 sm:pr-6 lg:pl-8 lg:pr-8 text-left">
               <Link href="/om-oss">
-                <span className="title block cursor-pointer text-8xl mb-4">Om Oss</span>
+                <span className={`title block cursor-pointer text-8xl mb-4 ${isTextWhite ? 'text-white' : 'text-black'}`}>Om Oss</span>
               </Link>
               <Link href="/siste-nytt">
-                <span className="title block cursor-pointer text-8xl mb-4">Siste nytt</span>
+                <span className={`title block cursor-pointer text-8xl mb-4 ${isTextWhite ? 'text-white' : 'text-black'}`}>Siste nytt</span>
               </Link>
               <Link href="/prosjekter">
-                <span className="title block cursor-pointer text-8xl mb-4">Nedvekst</span>
+                <span className={`title block cursor-pointer text-8xl mb-4 ${isTextWhite ? 'text-white' : 'text-black'}`}>Nedvekst</span>
               </Link>
             </div>
             <div className="absolute bottom-4 left-4 right-4 flex justify-between items-end">
               <Link href="/kontakt">
-                <span className="cursor-pointer text-4xl">Kontakt</span>
+                <span className={`cursor-pointer text-4xl ${isTextWhite ? 'text-white' : 'text-black'}`}>Kontakt</span>
               </Link>
               <Link href="/nyhetsbrev">
-                <span className="cursor-pointer text-4xl">Nyhetsbrev</span>
+                <span className={`cursor-pointer text-4xl ${isTextWhite ? 'text-white' : 'text-black'}`}>Nyhetsbrev</span>
               </Link>
             </div>
           </motion.div>
