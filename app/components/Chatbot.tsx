adc475a36afd8525ee9e1de24affd409f0e991fa
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-12-27 20:09:29 +0000

    sure

--- app/components/Chatbot.tsx
+++ app/components/Chatbot.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { Send } from 'lucide-react';
 
 // Define a type for the messages
 type Message = {
@@ -13,22 +14,8 @@ const Chatbot = () => {
   const [input, setInput] = useState('');
 
   // Custom system message prompt
-  const systemMessage = `
-  - du er Robot er en informasjonsassistent for Robust. aldri svar lenger enn. 2 setnigner!
-  - Svar skal være korte, informative og fokusert på Robust, dens prosjekter, og nedvekst-tiltak.
-  - Unngå fagsjargong og bruk klar, lettfattelig språk.
-  - svar kort og konsist, til meldinger som "hei" svar kort noe som 'hei! hva kan jeg hjelpe deg med'
-  - Informer om Robusts virksomhet, nedvekst og bærekraftig utvikling.
-  - Veiled til relevante ressurser, publikasjoner og svar på vanlige spørsmål.
-  - Tilby kontaktinformasjon og videre veiledning.
-  - Vær responsiv og brukervennlig, og gjenkjenn forespørsler om spesifikke sider eller dokumenter.
-  - Oppdateres regelmessig med ny informasjon om Robusts aktiviteter.
-  - Integreres med nettstedets eksisterende struktur og er tilgjengelig på alle sider.
-  - Hvis svaret er ukjent, bruk standardmeldingen: 'Jeg har ikke informasjon om dette emnet.'
+  const systemMessage = `Du er en AI-chatbot for nettsiden Robust.`;
 
-  <Foreningen Robust, som arbeider for en regenerativ økonomi innenfor planetens tålegrenser, søker støtte for en visuell og tekstbasert rapport, "Veiviser mot velvære innenfor planetens tålegrenser". Rapporten fokuserer på nedvekst-tiltak og utvikling av en økonomi som prioriterer velvære og beskytter planetens økosystemer. Den tar for seg konsepter som smultringøkonomi og nedvekst, og presenterer ideer for økonomisk systemendring gjennom visuelle virkemidler som infografikk og illustrasjoner, samt refleksjonsøvelser. Målet er å fremme offentlig diskusjon om sosio-økologisk omstilling og tilby nye perspektiver på økonomisk vekst, velstand og bærekraftig utvikling. Robust består av medlemmer fra diverse fagfelt og er involvert i nettverk som International Degrowth Network, og ønsker å formidle vitenskapelig forankrede alternativer til dagens økonomiske modeller>
-  `;
-  
   const handleSendMessage = async (content: string) => {
     if (!content.trim()) return;
 
@@ -73,23 +60,23 @@ const Chatbot = () => {
               className="relative bg-white rounded-lg shadow-xl p-4 max-w-sm w-full"
               onClick={(e) => e.stopPropagation()}
             >
-              <button className="absolute top-2 right-2 text-white" onClick={() => setIsOpen(false)}>
-                X {/* Replace with 'X' icon */}
-              </button>
+              <button className="absolute top-2 right-2 text-white" onClick={() => setIsOpen(false)}>X</button>
               <div className="overflow-y-auto h-64">
                 {messages.map((message, index) => (
-                  <div key={index} className={`message ${message.role}`}>{message.content}</div>
+                  <div key={index} className={`message ${message.role} ${message.role === 'user' ? 'bg-blue-100' : 'bg-gray-200'} m-2 p-2 rounded-lg`}>{message.content}</div>
                 ))}
               </div>
-              <form onSubmit={(e) => { e.preventDefault(); handleSendMessage(input); }}>
+              <form onSubmit={(e) => { e.preventDefault(); handleSendMessage(input); }} className="flex items-center">
                 <input
                   type="text"
                   value={input}
                   onChange={(e) => setInput(e.target.value)}
-                  className="w-full border-2 p-2 my-2"
+                  className="w-full border-2 p-2 my-2 rounded-l-lg"
                   placeholder="Type your message here..."
                 />
-                <button type="submit" className="p-2 bg-blue-500 w-full">Send</button>
+                <button type="submit" className="p-2 bg-blue-500 rounded-r-lg">
+                  <Send className="text-white" />
+                </button>
               </form>
             </motion.div>
           </div>
