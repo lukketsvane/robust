5a770ef923e3ef9cd2fdbe8b5aecf341fb8633a9
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-12-28 22:05:45 +0000

    artQ

--- pages/articles/[title].tsx
+++ pages/articles/[title].tsx
@@ -1,7 +1,7 @@
 import ArticleTemplate from '../../app/components/ArticleTemplate';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import React from 'react';
-import { getSortedArticlesData, ArticleData } from '../../lib/articles';
+import { getSortedArticlesData, ArticleData as BaseArticleData } from '../../lib/articles';
 import path from 'path';
 import fs from 'fs';
 import matter from 'gray-matter';
@@ -9,6 +9,12 @@ import { serialize } from 'next-mdx-remote/serialize';
 import { MDXRemote } from 'next-mdx-remote';
 
 const articlesDirectory = path.join(process.cwd(), 'articles');
+
+// Extending ArticleData to ensure it matches the expected structure for ArticleTemplate
+interface ArticleData extends BaseArticleData {
+  image: string; // Add any other missing properties that ArticleTemplate expects
+  // ... other properties like 'author', 'tags', etc.
+}
 
 interface ArticleProps {
   articleData: ArticleData;
@@ -26,10 +32,9 @@ const Article: React.FC<ArticleProps> = ({ articleData, mdxSource }) => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const allArticles = getSortedArticlesData();
   const formattedTitle = params?.title || '';
-  const articleData =
-    allArticles.find(
-      (data) => data.title.replace(/\s+/g, '-').toLowerCase() === formattedTitle
-    ) || null;
+  const articleData = allArticles.find(
+    (data) => data.title.replace(/\s+/g, '-').toLowerCase() === formattedTitle
+  ) as ArticleData | null; // Cast to ArticleData type
 
   let mdxSource;
 
@@ -38,7 +43,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     const fileContents = fs.readFileSync(fullPath, 'utf8');
     const { content } = matter(fileContents);
 
-    // Serialize the MDX content
     mdxSource = await serialize(content);
   } else {
     mdxSource = await serialize('<p>Article not found.</p>');
