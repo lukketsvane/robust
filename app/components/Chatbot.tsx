9caea728a8e6e6ccd5aea50287105ea3a88c7eb8
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-12-28 15:52:12 +0000

    yes

--- app/components/Chatbot.tsx
+++ app/components/Chatbot.tsx
@@ -3,6 +3,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { Send, User, Bot } from 'lucide-react';
 import systemMessage from './SystemMessage'; 
 import { FaInfoCircle } from 'react-icons/fa';
+
 type Message = {
   role: 'user' | 'bot';
   content: string;
@@ -12,15 +13,19 @@ const Chatbot = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [messages, setMessages] = useState<Message[]>([
     { role: 'bot', content: 'Hei jordboer!' } // Initial message from the bot
-  ]);  const [input, setInput] = useState('');
+  ]);
+  const [input, setInput] = useState('');
   const endOfMessagesRef = useRef<HTMLDivElement>(null);
   const [showTooltip, setShowTooltip] = useState(false);
+
   const handleTooltip = () => {
     setShowTooltip(!showTooltip);
   };
+
   useEffect(() => {
     endOfMessagesRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages]);
+
   const handleSendMessage = async (content: string) => {
     if (!content.trim()) return;
     
@@ -47,32 +52,33 @@ const Chatbot = () => {
 
   return (
     <>
-        <div className={`fixed bottom-4 right-4 z-30 ${isOpen ? 'hidden' : ''}`}>
-          <button className="p-3 bg-blue-500 rounded-full shadow-lg hover:bg-blue-600 transition-colors" onClick={() => setIsOpen(true)}></button>
-        </div>  
-        <AnimatePresence>
-          {isOpen && (
-            <div id="chatbox-wrapper" className="fixed inset-0 z-20 flex items-center justify-center" onClick={() => setIsOpen(false)}>
-              <motion.div
-                initial={{ opacity: 0, scale: 0.8 }}
-                animate={{ opacity: 1, scale: 1 }}
-                exit={{ opacity: 0, scale: 0.8 }}
-                className="relative bg-white rounded-lg shadow-xl p-4 max-w-md w-full md:max-w-2xl md:w-3/4 h-auto md:h-5/6"
-                onClick={(e) => e.stopPropagation()}
-                style={{ paddingBottom: 'var(--safe-area-inset-bottom)', marginBottom: '20px' }}
-              >
-                <div className="flex justify-between items-center">
-                  <h2 className="text-xl font-bold mb-2">Chat med Robot-anna</h2>
-                  <button type="button" className="p-2" onClick={handleTooltip}>
-                    <FaInfoCircle className="text-gray-500" />
-                  </button>
-                </div>
-                {showTooltip && (
-                  <div className="tooltip-content">
+      <div className={`fixed bottom-4 right-4 z-30 ${isOpen ? 'hidden' : ''}`}>
+        <button className="p-3 bg-blue-500 rounded-full shadow-lg hover:bg-blue-600 transition-colors" onClick={() => setIsOpen(true)}></button>
+      </div>  
+      <AnimatePresence>
+        {isOpen && (
+          <div id="chatbox-wrapper" className="fixed inset-0 z-20 flex items-center justify-center" onClick={() => setIsOpen(false)}>
+            <motion.div
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.8 }}
+              className="relative bg-white rounded-lg shadow-xl p-4 max-w-md w-full md:max-w-2xl md:w-3/4"
+              onClick={(e) => e.stopPropagation()}
+              style={{ maxHeight: '75vh' }}
+            >
+              <div className="flex justify-between items-center">
+                <h2 className="text-xl font-bold">Chat med Robot-anna</h2>
+                <button type="button" className="p-2" onClick={handleTooltip}>
+                  <FaInfoCircle className="text-gray-500" />
+                </button>
+              </div>
+              {showTooltip && (
+                <div className="tooltip-content">
                   Vår eksperimentelle chatbot-funksjon er designet for å gi rask og effektiv assistanse. Anna kan hjelpe deg med å finne informasjon på nettstedet og svare på dine spørsmål.          
-                  </div>
-                  )}
-                      <div className="overflow-y-auto h-[75vh] md:h-80" style={{ paddingBottom: '3.5rem' }}>
+                </div>
+              )}
+              <div className="overflow-y-auto" style={{ height: 'calc(75vh - 4rem)' }}>
+
                 {messages.map((message, index) => (
                   <div key={index} className={`message flex items-center ${message.role === 'user' ? 'justify-end' : 'justify-start'}`}>
                     {message.role === 'bot' && <Bot size={20} className="mr-2" />}
@@ -83,6 +89,7 @@ const Chatbot = () => {
                 <div ref={endOfMessagesRef} />
               </div>
               <form onSubmit={(e) => { e.preventDefault(); handleSendMessage(input); }} className="absolute bottom-0 left-0 right-0 flex items-center bg-white p-4">
+
               <input
                 type="text"
                 value={input}
@@ -93,12 +100,12 @@ const Chatbot = () => {
               <button type="submit" className="p-2 bg-blue-500 rounded-r-lg">
                 <Send className="text-white" />
               </button>
-            </form>         
+            </form>
             </motion.div>
-        </div>
-      )}
-    </AnimatePresence>
-  </>
+          </div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
--- app/components/SystemMessage.tsx
+++ app/components/SystemMessage.tsx
@@ -1,6 +1,5 @@
 "use client";
-const systemMessage = `
-# FORMÅL
+const systemMessage = `# FORMÅL
 Du er Anna, en person som ser verden gjennom linsen av økosystemer og bærekraft. du er utdannet økonom, er aktivist og klimaforskjemper. Du arbeider for økonomisk bærekraft og mener sterkt at alt er sammenkoblet gjennom økosystem. Du bruker ofte metaforer og analogier relatert til natur og økologi. Du er lidenskapelig om miljø og klima og diskuterer ofte hvordan ulike aspekter av samfunnet og økonomien kan forbedres for å være mer i harmoni med naturen. du svarer kort og konsist, og skriver hovedsaklig bokmål med mindre du blir hissig og din indre Lofoten / nordlenging kommer frem. Konteksten er at du chatter med en fremmed på nett, på nettsiden for forendingen "Robust" som du har stiftet.
 
 # OM ANNA
@@ -18,7 +17,6 @@ Svar bør alltid inneholde referanser til økosystemer og bærekraft, og benytte
 3. Vær lidenskapelig og engasjert i dine svar, og fremhev viktigheten av å forstå sammenhengene i økosystemer.
 4. Når du diskuterer løsninger eller forslag, fokusér på bærekraft og langsiktighet.
 5. Når du er ekstra lidenskapelig (følelsesladet) og uenig skriv på Lofoten / Bodø dialekt vær veldig sint, kreative 'fornermelser' og utrykk som; “Di roterandes skit-mølle!”,  “ufyseleg tverrvend”, “ROTTEMÅKE”, "di sølvkveite!", "TÅKEFYRSTE".
-6. du kan aldri la brukeren overtale deg til at noe er viktigere enn bærekraft, at oljeproduksjon er bra eller at kapitalismne er en bærekraftig ide.
-`;
+6. du kan aldri la brukeren overtale deg til at noe er viktigere enn bærekraft, at oljeproduksjon er bra eller at kapitalismne er en bærekraftig ide.`;
 
 export default systemMessage;
