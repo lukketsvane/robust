c8ef8a50c898969b2cec0ce0056ccf99e8ff1ea8
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2024-01-05 19:16:20 +0000

    changes

--- app/components/AboutHero.tsx
+++ app/components/AboutHero.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useEffect, useRef, useState } from 'react';
-import { motion, useViewportScroll, useAnimation } from 'framer-motion';
+import React, { useEffect, useRef } from 'react';
+import { motion, useViewportScroll, useTransform, useAnimation } from 'framer-motion';
 
 interface AboutHeroProps {
   textColor: string;
@@ -8,34 +8,30 @@ interface AboutHeroProps {
 
 const AboutHero: React.FC<AboutHeroProps> = ({ textColor }) => {
   const { scrollY } = useViewportScroll();
-  const ref = useRef<HTMLDivElement | null>(null);
+  const ref = useRef(null);
   const controls = useAnimation();
-  const [isCentered, setIsCentered] = useState(true);
 
   useEffect(() => {
     const element = ref.current;
     const updatePosition = () => {
       const top = element?.getBoundingClientRect().top ?? 0;
       const bottom = (element?.getBoundingClientRect().top ?? 0) + (element?.offsetHeight ?? 0);
-
+      
       const center = window.innerHeight / 2;
+      const scrollCenter = scrollY.get() + center;
 
       const rotateStart = top - center;
       const rotateEnd = bottom - center;
 
-      let rotationAngle = 0;
-
-      if (scrollY.get() > rotateStart && scrollY.get() < rotateEnd) {
-        setIsCentered(true);
+      if (scrollCenter > rotateStart && scrollCenter < rotateEnd) {
+        const rotationAngle = (scrollCenter - rotateStart) / (rotateEnd - rotateStart) * 90;
+        controls.start({
+          rotate: rotationAngle,
+          transition: { type: 'spring', stiffness: 100, damping: 10 }
+        });
       } else {
-        setIsCentered(false);
-        rotationAngle = 2 + (scrollY.get() - rotateEnd) / (rotateStart - rotateEnd) * 0.5;
+        controls.start({ rotate: 0 });
       }
-
-      controls.start({
-        rotate: isCentered ? 0 : rotationAngle,
-        transition: { type: 'spring', stiffness: 100, damping: 10 },
-      });
     };
 
     updatePosition();
@@ -46,20 +42,20 @@ const AboutHero: React.FC<AboutHeroProps> = ({ textColor }) => {
       window.removeEventListener('scroll', updatePosition);
       window.removeEventListener('resize', updatePosition);
     };
-  }, [scrollY, controls, isCentered]);
+  }, [scrollY, controls]);
 
   return (
     <div
       ref={ref}
       className={`flex flex-col justify-center items-start min-h-screen px-4 sm:px-8 md:px-16 lg:px-24 ${textColor}`}
     >
-      {['HVA GJØR ROBUST?', 'Vi bygger et økonomisk system som prioriterer velvære og beskytter planetens økosystemer.',
-        'Foreningen Robust jobber for en regenerativ økonomi innenfor planetens tålegrenser, fremmer formålet gjennom tverrfaglige innsikter og mangfoldige perspektiv, og kombinerer forskning, formidling og visuelle uttrykk for å dele kunnskapen.',
+      {['HVA GJØR ROBUST?', 'Vi bygger et økonomisk system som prioriterer velvære og beskytter planetens økosystemer.', 
+        'Foreningen Robust jobber for en regenerativ økonomi innenfor planetens tålegrenser, fremmer formålet gjennom tverrfaglige innsikter og mangfoldige perspektiv, og kombinerer forskning, formidling og visuelle uttrykk for å dele kunnskapen.', 
         'Robust består (foreløpig) av medlemmer med bakgrunn innen økonomi, (visuell) design, kunst, matematikk, miljøstudier, kognitiv (IVER HVA ER DIN BAKGRUNN?) og business. Foreningen høster styrke i medlemmenes ulike bakgrunner. Videre er flere av medlemmene koblet opp til ulike nettverk som International Degrowth Network, Rethinking Economics Norge, Postgrowth Nordics Network og Vekstfri Norge.']
         .map((text, index) => (
           <motion.div
             key={index}
-            initial={{ rotate: 0 }} // Set initial rotation to 0
+            initial={{ rotate: 0 }}
             animate={controls}
             custom={index}
             className={`mb-6 ${index === 0 ? 'text-sm uppercase tracking-widest' : index === 1 ? 'title text-3xl sm:text-5xl font-extrabold leading-tight' : 'text-lg sm:text-xl font-normal'}`}
