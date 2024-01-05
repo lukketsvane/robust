76940f21549f01a42d7d6f2cc2d35052055a1adb
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2024-01-05 18:47:34 +0000

    changes

--- app/components/AboutHero.tsx
+++ app/components/AboutHero.tsx
@@ -1,38 +1,63 @@
-"use client";
-import React from 'react';
-import { motion, useViewportScroll, useTransform } from 'framer-motion';
+import React, { useEffect, useRef } from 'react';
+import { motion, useViewportScroll, useTransform, useAnimation } from 'framer-motion';
 
 interface AboutHeroProps {
   textColor: string;
 }
 
 const AboutHero: React.FC<AboutHeroProps> = ({ textColor }) => {
-  const { scrollYProgress } = useViewportScroll();
+  const { scrollY } = useViewportScroll();
+  const ref = useRef(null);
+  const controls = useAnimation();
 
-  const yRange = useTransform(scrollYProgress, [0, 0.1], ['300px', '0px']);
-  const opacityRange = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
+  useEffect(() => {
+    const element = ref.current;
+    const updatePosition = () => {
+      const top = element?.getBoundingClientRect().top ?? 0;
+      const bottom = (element?.getBoundingClientRect().top ?? 0) + (element?.offsetHeight ?? 0);
+      
+      const rotateStart = top - window.innerHeight;
+      const rotateEnd = bottom - window.innerHeight;
+      
+      if (scrollY.get() > rotateStart && scrollY.get() < rotateEnd) {
+        controls.start(i => ({
+          rotate: 0,
+          transition: { delay: i * 0.1, type: 'spring', stiffness: 100, damping: 10 }
+        }));
+      }
+    };
 
-  const transition = { duration: 1, type: 'spring' };
+    updatePosition();
+    window.addEventListener('scroll', updatePosition);
+    window.addEventListener('resize', updatePosition);
+
+    return () => {
+      window.removeEventListener('scroll', updatePosition);
+      window.removeEventListener('resize', updatePosition);
+    };
+  }, [scrollY, controls]);
 
   return (
-    <motion.div
-      initial={{ y: '300px', opacity: 0 }}
-      style={{ y: yRange, opacity: opacityRange }}
-      transition={transition}
+    <div
+      ref={ref}
       className={`flex flex-col justify-center items-start min-h-screen px-4 sm:px-8 md:px-16 lg:px-24 ${textColor}`}
     >
-      <h2 className={`text-sm uppercase tracking-widest mb-6 ${textColor}`}>HVA GJØR ROBUST?</h2>
-      <p className={`title text-3xl sm:text-5xl font-extrabold leading-tight mb-6 ${textColor}`}>
-        Vi bygger et økonomisk system som prioriterer velvære og beskytter planetens økosystemer.
-      </p>
-      <p className={`text-lg sm:text-xl font-normal ${textColor}`}>
-        Foreningen Robust jobber for en regenerativ økonomi innenfor planetens tålegrenser, fremmer formålet gjennom tverrfaglige innsikter og mangfoldige perspektiv, og kombinerer forskning, formidling og visuelle uttrykk for å dele kunnskapen.
-      </p>
-      <br />
-      <p className={`text-lg sm:text-xl font-normal ${textColor}`}>
-        Robust består (foreløpig) av medlemmer med bakgrunn innen økonomi, (visuell) design, kunst, matematikk, miljøstudier, kognitiv (IVER HVA ER DIN BAKGRUNN?) og business. Foreningen høster styrke i medlemmenes ulike bakgrunner. Videre er flere av medlemmene koblet opp til ulike nettverk som International Degrowth Network, Rethinking Economics Norge, Postgrowth Nordics Network og Vekstfri Norge.
-      </p>
-    </motion.div>
+      {['HVA GJØR ROBUST?', 'Vi bygger et økonomisk system som prioriterer velvære og beskytter planetens økosystemer.', 
+        'Foreningen Robust jobber for en regenerativ økonomi innenfor planetens tålegrenser, fremmer formålet gjennom tverrfaglige innsikter og mangfoldige perspektiv, og kombinerer forskning, formidling og visuelle uttrykk for å dele kunnskapen.', 
+        'Robust består (foreløpig) av medlemmer med bakgrunn innen økonomi, (visuell) design, kunst, matematikk, miljøstudier, kognitiv (IVER HVA ER DIN BAKGRUNN?) og business. Foreningen høster styrke i medlemmenes ulike bakgrunner. Videre er flere av medlemmene koblet opp til ulike nettverk som International Degrowth Network, Rethinking Economics Norge, Postgrowth Nordics Network og Vekstfri Norge.']
+        .map((text, index) => (
+          <motion.div
+            key={index}
+            initial={{ rotate: 2 + index * 0.5 }} // Incremental initial rotation for each text
+            animate={controls}
+            custom={index} // Pass the index as a custom prop to use in the animation control
+            className={`mb-6 ${index === 0 ? 'text-sm uppercase tracking-widest' : index === 1 ? 'title text-3xl sm:text-5xl font-extrabold leading-tight' : 'text-lg sm:text-xl font-normal'}`}
+          >
+            {text}
+          </motion.div>
+        ))
+      }
+    </div>
   );
 };
 
